31381fc2be6443fb96f36b5a7c82965a69ec5eab
mashilong <745030904@qq.com>
2017-11-09 16:31:34 +0800

    中介者模式

---
+++ src/action/mediator/AbstractMediator.java
@@ -0,0 +1,22 @@
+package action.mediator;
+
+/**
+ * 中介者
+ */
+public abstract class AbstractMediator {
+	protected AbstractColleague A;
+	protected AbstractColleague B;
+
+	public AbstractMediator(AbstractColleague a, AbstractColleague b) {
+		A = a;
+		B = b;
+	}
+
+	protected AbstractMediator() {
+	}
+
+	public abstract void AaffectB();
+
+	public abstract void BaffectA();
+
+}
